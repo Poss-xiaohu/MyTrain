bc253ed10e5cf93074fa350fd7d7977ecb3cd9b8
zh <zh_poss@163.com>
2023-11-15 20:43:46 +0800

    集成PageHelper,实现SQL分页功能

--- member/src/main/java/com/poss/mapper/PassengerMapper.java
+++ member/src/main/java/com/poss/mapper/PassengerMapper.java
@@ -3,8 +3,10 @@ package com.poss.mapper;
 import com.poss.domain.Passenger;
 import com.poss.domain.PassengerExample;
 import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+@Mapper
 public interface PassengerMapper {
     long countByExample(PassengerExample example);
 
