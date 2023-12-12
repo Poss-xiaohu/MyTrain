e0a2725e90ceb56fd27f2804d5eca8d96005a068
zh <zh_poss@163.com>
2023-12-12 22:08:45 +0800

    火车车次下拉选择功能开发，增加后端接口

---
+++ business/src/main/java/com/poss/controller/admin/TrainSeatAdminController.java
@@ -0,0 +1,39 @@
+package com.poss.controller.admin;
+
+import com.poss.context.LoginMemberContext;
+import com.poss.resp.CommonResp;
+import com.poss.resp.PageResp;
+import com.poss.req.TrainSeatQueryReq;
+import com.poss.req.TrainSeatSaveReq;
+import com.poss.resp.TrainSeatQueryResp;
+import com.poss.service.TrainSeatService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/train-seat")
+public class TrainSeatAdminController {
+
+    @Resource
+    private TrainSeatService trainSeatService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody TrainSeatSaveReq req) {
+        trainSeatService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<TrainSeatQueryResp>> queryList(@Valid TrainSeatQueryReq req) {
+        PageResp<TrainSeatQueryResp> list = trainSeatService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        trainSeatService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
