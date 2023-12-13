c3bd789f08f497854b65313fc5627be2d1d22ece
zh <zh_poss@163.com>
2023-12-13 22:30:41 +0800

    为火车车厢页面增加车次查询条件，注意null和""的区别。

--- business/src/main/java/com/poss/req/TrainCarriageQueryReq.java
+++ business/src/main/java/com/poss/req/TrainCarriageQueryReq.java
@@ -2,10 +2,22 @@ package com.poss.req;
 
 
 public class TrainCarriageQueryReq extends PageReq {
+    
+    private String trainCode;
 
     @Override
     public String toString() {
         return "TrainCarriageQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
 }
