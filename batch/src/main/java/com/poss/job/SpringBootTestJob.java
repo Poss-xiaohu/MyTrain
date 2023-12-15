2f4651d16491109b0938669c761836cfde3f3ba8
zh <zh_poss@163.com>
2023-12-15 22:47:20 +0800

    进制任务并发执行。

--- batch/src/main/java/com/poss/job/SpringBootTestJob.java
+++ batch/src/main/java/com/poss/job/SpringBootTestJob.java
@@ -8,13 +8,13 @@ package com.poss.job;
   * 适合单体应用，不适合集群
   * 没法实时更改定时任务状态和策略
   */
- @Component
- @EnableScheduling
- public class SpringBootTestJob {
-
-     @Scheduled(cron = "0/5 * * * * ?")
-     private void test() {
-         // 增加分布式锁，解决集群问题
-         System.out.println("SpringBootTestJob TEST");
-     }
- }
+// @Component
+// @EnableScheduling
+// public class SpringBootTestJob {
+//
+//     @Scheduled(cron = "0/5 * * * * ?")
+//     private void test() {
+//         // 增加分布式锁，解决集群问题
+//         System.out.println("SpringBootTestJob TEST");
+//     }
+// }
