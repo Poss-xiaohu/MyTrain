4035113dd31e1a8161723b9178bd48ddfa72b204
zh <zh_poss@163.com>
2023-12-18 20:51:19 +0800

    集成openfeign，实现服务间调用。给每个应用增加应用名，可搭配配置中心使用。

--- batch/src/main/java/com/poss/BatchApplication.java
+++ batch/src/main/java/com/poss/BatchApplication.java
@@ -2,8 +2,10 @@ package com.poss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients({"com.poss.feign"})
 public class BatchApplication {
 
     public static void main(String[] args) {
--- batch/src/main/java/com/poss/controller/TestController.java
+++ batch/src/main/java/com/poss/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.poss.controller;
 
+import com.poss.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class TestController {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+    
+    @Resource
+    BusinessFeign businessFeign;
 
     @GetMapping("/hello")
     public String testController(){
+        String businessHello = businessFeign.hello();
+        LOG.info(businessHello);
         return "hello world!!!  Batch!";
     }
 }
