024410ee3b32dbd79c4098e588db0cb9bd04ff26
zh <zh_poss@163.com>
2023-12-18 19:28:33 +0800

    修改每日座位页面，增加车次查询条件，修改列号的显示。

--- business/src/main/java/com/poss/req/DailyTrainSeatQueryReq.java
+++ business/src/main/java/com/poss/req/DailyTrainSeatQueryReq.java
@@ -3,9 +3,20 @@ package com.poss.req;
 
 public class DailyTrainSeatQueryReq extends PageReq {
 
+    private String trainCode;
+
     @Override
     public String toString() {
         return "DailyTrainSeatQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
     }
 }
