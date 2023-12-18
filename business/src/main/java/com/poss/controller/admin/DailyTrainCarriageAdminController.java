bb2dadcc36559d6ccbcd5514066af04f2f25606e
zh <zh_poss@163.com>
2023-12-18 18:56:08 +0800

    快速生成每日车厢数据管理功能。

---
+++ business/src/main/java/com/poss/controller/admin/DailyTrainCarriageAdminController.java
@@ -0,0 +1,39 @@
+package com.poss.controller.admin;
+
+import com.poss.context.LoginMemberContext;
+import com.poss.resp.CommonResp;
+import com.poss.resp.PageResp;
+import com.poss.req.DailyTrainCarriageQueryReq;
+import com.poss.req.DailyTrainCarriageSaveReq;
+import com.poss.resp.DailyTrainCarriageQueryResp;
+import com.poss.service.DailyTrainCarriageService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/daily-train-carriage")
+public class DailyTrainCarriageAdminController {
+
+    @Resource
+    private DailyTrainCarriageService dailyTrainCarriageService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainCarriageSaveReq req) {
+        dailyTrainCarriageService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainCarriageQueryResp>> queryList(@Valid DailyTrainCarriageQueryReq req) {
+        PageResp<DailyTrainCarriageQueryResp> list = dailyTrainCarriageService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainCarriageService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ business/src/main/java/com/poss/req/DailyTrainCarriageQueryReq.java
@@ -0,0 +1,11 @@
+package com.poss.req;
+
+
+public class DailyTrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainCarriageQueryReq{" +
+                "} " + super.toString();
+    }
+}
